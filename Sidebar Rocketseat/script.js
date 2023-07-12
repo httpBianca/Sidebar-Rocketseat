c00e999a5f5038fcfbc84f7395fa3e024429bf91
Bianca Oliveira <113205824+httpBianca@users.noreply.github.com>
2023-07-11 21:33:42 -0300

    Delete Sidebar Rocketseat directory

--- Sidebar Rocketseat/script.js
+++
@@ -1,8 +0,0 @@
-const menu = document.querySelector('#menu');
-const header = document.querySelector('header');
-
-menu.addEventListener('click', fecharHeader);
-
-function fecharHeader(){
-    header.classList.toggle('close');
-}
